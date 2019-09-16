44b7e37897071a49aef2adf95fb6b43f8c2923aa
SK <sankar.beprofessional@gmail.com>
2019-09-16 18:41:48 +0530

    Test Commit
    Signed-off-by: SK <sankar.beprofessional@gmail.com>

---
+++ src/main/java/tests/NewTest.java
@@ -0,0 +1,26 @@
+package tests;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	public WebDriver driver;
+	
+  @Test
+  public void openmyblog() {
+	  driver.get("http://sankarqtpautomation.blogspot.com/");
+  }
+  
+  @BeforeClass
+  public void beforeClass() {
+	 System.setProperty("webdriver.chrome.driver","R:\\PythonSeleniumBDD\\driver\\chromedriver.exe");
+	 driver = new ChromeDriver();
+  }
+  @AfterClass
+  public void AfterClass() {
+	  driver.quit();
+  }
+}
+   
